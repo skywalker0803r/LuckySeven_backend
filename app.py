1193141b65fac416746e06e52caef36bb2e77ab6
skywalker0803r <skywalker0803r@example.com>
2025-07-24 20:24:00 +0800

    準備上線

--- app.py
+++ app.py
@@ -104,8 +104,8 @@ app = FastAPI()
 # CORS settings
 app.add_middleware(
     CORSMiddleware,
-    #allow_origins=["https://luckyseven-frontend.onrender.com"],
-    allow_origins=["*"],
+    allow_origins=["https://luckyseven-frontend.onrender.com"],
+    #allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
